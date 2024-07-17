1500a57cfb483897c78ad962080abe8568bb0e25
Vishesh Verma <visheshverma250703@gmail.com>
2024-07-17 17:58:53 +0530

    day 2 completed

---
+++ day2.js
@@ -0,0 +1,58 @@
+// Task/Activities
+// Activity 1: Artithmetic Operations
+let a = 5, b = 20;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+//updating numbers
+a+=b; // a = 25
+b-=a; // b = -5
+console.log("after Addition:", a);
+console.log("after Subtraction:", b);
+
+// Activity 2: Comparison Operators
+console.log("------------------------------------------------------------------------------------------------------");
+a = 5, b = 20;
+console.log("Equal to:", a == b);
+console.log("Not Equal to:", a != b);
+console.log("Greater than:", a > b);
+console.log("Less than:", a < b);
+
+// Activity 3: Logical Operators
+console.log("------------------------------------------------------------------------------------------------------");
+let c = true, d = false;
+console.log("AND:", c && d);
+console.log("OR:", c || d);
+console.log("NOT:", !c);
+
+// Activity 4: Ternary Operator
+console.log("------------------------------------------------------------------------------------------------------");
+let e = 10, f = 20;
+let result = (e > f) ? "Greater" : "Lesser";
+console.log(result);
+
+
+//Feature Request
+console.log("**********************************************************************************************************");
+//task 1 Artihmetic Operations
+let g = 5, h = 10;
+console.log("Addition:", g + h);
+console.log("Subtraction:", g - h);
+console.log("Multiplication:", g * h);
+console.log("Division:", g / h);
+console.log("Modulus:", g % h);
+
+//task 2 Comparison Operators
+console.log("------------------------------------------------------------------------------------------------------");
+g = 5, h = 10;
+console.log("Equal to:", g == h);
+console.log("Not Equal to:", g != h);
+console.log("Greater than:", g > h);
+console.log("Less than:", g < h);
+
+//task 3 Ternary Operators
+console.log("------------------------------------------------------------------------------------------------------");
+let i = 10;
+console.log((i>0) ? "Positive" : "Negative");
