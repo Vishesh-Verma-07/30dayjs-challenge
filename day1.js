4abba74cbc895dc5855b1a69d998fd86398065e2
Vishesh Verma <visheshverma250703@gmail.com>
2024-07-17 17:30:39 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,87 @@
+// Task/Activities
+// Activity 1: Variable Declartion
+var a = 2;
+let b = "vishesh";
+console.log(a);
+console.log(b);
+
+// Activity 2: Constant Declaration
+console.log("---------------------------------------------------");
+const c = true;
+console.log(c);
+
+// Activity 3: Data Types
+console.log("---------------------------------------------------");
+let d = 2;
+let e = 'vishesh';
+let f = true;
+let g = {name: 'vishesh', age: 21};
+let h = [1,2,3,4,5];
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+
+// Activity 4: Reassigning Variables
+console.log("---------------------------------------------------");
+let i = 2;
+console.log(i);
+i = 3;
+console.log(i);
+
+
+// Activity 5: Understanding Constants
+console.log("---------------------------------------------------");
+const j = 2;
+console.log(j);
+j = 3;
+console.log(j);
+
+//Feature Request
+console.log("*****************************************************");
+
+// String
+let name = "Vishesh";
+console.log("String:", name);
+
+// Number
+let age = 21;
+console.log("Number:", age);
+
+// Boolean
+let isStudent = true;
+console.log("Boolean:", isStudent);
+
+// Undefined
+let job;
+console.log("Undefined:", job);
+
+// Null
+let car = null;
+console.log("Null:", car);
+
+// Object
+let person = {
+    firstName: "vishesh",
+    lastName: "verma"
+};
+console.log("Object:", person);
+
+// Array
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log("Array:", fruits);
+
+// Function
+function greet() {
+    return "Hello, World!";
+}
+console.log("Function:", greet);
+
+// Symbol
+let symbol = Symbol("unique");
+console.log("Symbol:", symbol);
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt:", bigNumber);
